f8646c0e155397aece1ed6be712d687840ab3e17
sojaehong <woghd4545@gmail.com>
2021-03-11 20:04:00 +0900

    - 설정 구성 작업중
    TODO
    
    - 통계
    - 할일 히스토리
    - 노트
    - 할일 간단 노트
    - 암호

---
+++ app/src/main/java/com/ssostudio/mytodo/fragment/AppReview.java
@@ -0,0 +1,13 @@
+package com.ssostudio.mytodo.fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+public class AppReview {
+    public static void onAppReview(Context context){
+        String appPackageName = "com.ssostudio.thanksdiary";
+        Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName);
+        context.startActivity(new Intent(Intent.ACTION_VIEW, uri).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+}
--- app/src/main/java/com/ssostudio/mytodo/fragment/SettingFragment.java
+++ app/src/main/java/com/ssostudio/mytodo/fragment/SettingFragment.java
@@ -8,18 +8,40 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
 import com.ssostudio.mytodo.R;
+import com.ssostudio.mytodo.utility.OtherApps;
 
-public class SettingFragment extends Fragment {
-
+public class SettingFragment extends Fragment implements View.OnClickListener {
     private static View view;
     private Context _context;
+    private MaterialButton otherAppsButton;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_setting, container, false);
         _context = getContext();
 
+        init();
+
         return view;
     }
 
+    private void init() {
+        otherAppsButton = view.findViewById(R.id.other_apps_button);
+        otherAppsButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.other_apps_button:
+                onOtherAppsBtnClick();
+                break;
+        }
+    }
+
+    private void onOtherAppsBtnClick(){
+        OtherApps.onOtherAppsShow(_context);
+    }
 }
